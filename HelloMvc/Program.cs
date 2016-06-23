ac7cabf9a3596519790919adcbba11e900efdcf8
David Zhao <dazhao@microsoft.com>
2016-06-22 17:27:57 -0700

    replace the content with WorksOnMyMachine

--- HelloMvc/Program.cs
+++ HelloMvc/Program.cs
@@ -1,21 +1,25 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
 
-namespace HelloMvc
+namespace WorksOnMyMachine
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            string port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-            
-            var host = new WebHostBuilder().UseUrls($"http://*:{port}")
-                                           .UseKestrel()
-                                           .UseContentRoot(Directory.GetCurrentDirectory())
-                                           .UseStartup<Startup>()
-                                           .Build();
+            string port = Environment.GetEnvironmentVariable("PORT") ?? "80";
+
+            var host = new WebHostBuilder()
+                .UseUrls($"http://*:{port}")
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
 
             host.Run();
         }
