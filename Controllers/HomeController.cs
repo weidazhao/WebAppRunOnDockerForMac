cad608b6e7f46dbfc52d7fe0290f8e9148477c2b
Weida Zhao <weida.zhao@hotmail.com>
2016-06-23 10:26:32 -0700

    formats file

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
 using System.Collections;
@@ -21,57 +18,58 @@ namespace WorksOnMyMachine.Controllers
 
             return View();
         }
-public IActionResult About()
-{
-    string hostName;
-    // Will only work on Windows
-    hostName = Environment.GetEnvironmentVariable("COMPUTERNAME");
 
-    // Works on both Windows and Linux
-    //hostName = Environment.GetEnvironmentVariable("COMPUTERNAME") ??
-    //                    Environment.GetEnvironmentVariable("HOSTNAME");
+        public IActionResult About()
+        {
+            string hostName;
+            // Will only work on Windows
+            hostName = Environment.GetEnvironmentVariable("COMPUTERNAME");
 
-    ViewData["HOSTNAME"] = hostName;
+            // Works on both Windows and Linux
+            //hostName = Environment.GetEnvironmentVariable("COMPUTERNAME") ??
+            //                    Environment.GetEnvironmentVariable("HOSTNAME");
 
-
-    TimeZoneInfo tzInfo = null;
-    // Will only work on Windows
-    tzInfo = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
-
-    // Works on both Windows and Linux
-    //if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-    //    tzInfo = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
-    //else
-    //    tzInfo = TimeZoneInfo.FindSystemTimeZoneById("America/New_York");
-
-    ViewData["TZINFO"] = tzInfo;
-
-    var OSArchitecture = RuntimeInformation.OSArchitecture;
-    ViewData["OSARCHITECTURE"] = OSArchitecture.ToString();
-
-    var OSDescription = RuntimeInformation.OSDescription;
-    ViewData["OSDESCRIPTION"]= OSDescription.ToString();
-
-    var ProcessArchitecture = RuntimeInformation.ProcessArchitecture;
-    ViewData["PROCESSARCHITECTURE"] = ProcessArchitecture.ToString();
-
-    string FrameworkDescription = RuntimeInformation.FrameworkDescription;
-    ViewData["FRAMEWORKDESCRIPTION"] = FrameworkDescription;
-
-    string hostingEnvironment = Environment.GetEnvironmentVariable("Hosting:Environment");
-    ViewData["HOSTING_ENVIRONMENT"] = hostingEnvironment;
-
-    StringBuilder envVars = new StringBuilder();
-    foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
-        envVars.Append(string.Format("<strong>{0}</strong>:{1}<br \\>", de.Key, de.Value));
-
-    ViewData["ENV_VARS"] = envVars.ToString();
+            ViewData["HOSTNAME"] = hostName;
 
 
-    ViewData["Message"] = "Your application description page.";
+            TimeZoneInfo tzInfo = null;
+            // Will only work on Windows
+            tzInfo = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
 
-    return View();
-}
+            // Works on both Windows and Linux
+            //if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            //    tzInfo = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            //else
+            //    tzInfo = TimeZoneInfo.FindSystemTimeZoneById("America/New_York");
+
+            ViewData["TZINFO"] = tzInfo;
+
+            var OSArchitecture = RuntimeInformation.OSArchitecture;
+            ViewData["OSARCHITECTURE"] = OSArchitecture.ToString();
+
+            var OSDescription = RuntimeInformation.OSDescription;
+            ViewData["OSDESCRIPTION"]= OSDescription.ToString();
+
+            var ProcessArchitecture = RuntimeInformation.ProcessArchitecture;
+            ViewData["PROCESSARCHITECTURE"] = ProcessArchitecture.ToString();
+
+            string FrameworkDescription = RuntimeInformation.FrameworkDescription;
+            ViewData["FRAMEWORKDESCRIPTION"] = FrameworkDescription;
+
+            string hostingEnvironment = Environment.GetEnvironmentVariable("Hosting:Environment");
+            ViewData["HOSTING_ENVIRONMENT"] = hostingEnvironment;
+
+            StringBuilder envVars = new StringBuilder();
+            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+                envVars.Append(string.Format("<strong>{0}</strong>:{1}<br \\>", de.Key, de.Value));
+
+            ViewData["ENV_VARS"] = envVars.ToString();
+
+
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
 
         public IActionResult Error()
         {
